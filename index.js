649da4a0233a3d834b8fba97a37358ec5a44257d
kopelthep <47904193+kopelthep@users.noreply.github.com>
2026-04-22 13:52:13 -0400

    Front end

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(cors())
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
 
 let notes = [
   {
