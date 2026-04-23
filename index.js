c257d6199d0500a8bc04762b492ae06c5b400559
kopelthep <47904193+kopelthep@users.noreply.github.com>
2026-04-23 14:18:08 -0400

    Adding post

--- index.js
+++ index.js
@@ -91,13 +91,17 @@ app.post('/api/notes', (request, response) => {
   const note = {
     content: body.content,
     important: body.important || false,
-    id: generateId(),
+    
   }
 
   notes = notes.concat(note)
 
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
+
+
 
 
 app.delete('/api/notes/:id', (request, response) => {
